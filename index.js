7203a448cf5d1e49c8fec330d2cfd0002067b09e
Alejandro Garcia Gasco Perez <algarcpe@everis.com>
2018-01-31 14:22:01 +0100

    petición get

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ var port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
+app.get("/prueba/:name?",function(req, res){
+
+    var user;
+    if(req.params.name){
+        user = req.params.name;
+    }else{
+        user = 'Anónimo'
+    }
+
+
+    res.status(200).send({message: 'Hola mundo '+ user})
+})
+
+
 app.listen(3000,function(){
-    console.log("Funcionando correctamente en http://localhost:"+port)
+    console.log("Funcionando correctamente en http://localhost: "+port)
 });
