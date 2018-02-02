69df7bca6a470b3c94fba4e41e01f847567761f6
Alejandro Garcia Gasco Perez <algarcpe@everis.com>
2018-02-02 13:59:14 +0100

    getall and save services

--- controllers/favoritoCtrl.js
+++ controllers/favoritoCtrl.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var favoritoModel = require('../models/favoritoModel');
+
 function prueba (req, res){
 
     var user;
@@ -20,13 +22,43 @@ function getFavorito(req,res){
 }
 
 function getFavoritos(req,res){
-
+    favoritoModel.find({}).sort('-title').exec(function(err,resp){
+        if(err){
+            res.status(500).send({message: "Error al devolver los marcadores"})
+        }
+        else{
+            if(!resp.length){
+                res.status(400).send({message: "No hay marcadores"})
+            }else{
+                res.status(200).send({data:resp})
+            }
+        }
+    })
 }
 
 function saveFavorito(req,res){
+    var favorito = new favoritoModel();
+
     var params = req.body;
 
-    res.status(200).send({data:params})
+    favorito.title= params.title;
+    favorito.description = params.description;
+    favorito.url = params.url;
+
+    console.log(favorito)
+
+
+    favorito.save(function(err,resp){
+        if(err){
+            res.status(500).send({message: "Error saving data"})
+        }
+        else{
+            res.status(200).send({data:resp})
+        }
+    })
+
+
+
 
 }
 
@@ -44,4 +76,4 @@ function deleteFavorito(req,res){
 }
 
 
-module.exports = {prueba, getFavorito, saveFavorito, updateFavorito, deleteFavorito}
+module.exports = {prueba, getFavorito, saveFavorito, updateFavorito, deleteFavorito, getFavoritos}
--- routes/rutas-favorito.js
+++ routes/rutas-favorito.js
@@ -1,7 +1,7 @@
 'use strict'
 
 var express = require('express');
-var favoritoController = require('../controllers/favoritoCtrl')
+var favoritoController = require('../controllers/favoritoCtrl');
 
 var api = express.Router();
 
@@ -9,6 +9,7 @@ var api = express.Router();
 
 api.get("/prueba/:name?",favoritoController.prueba)
 api.get("/favorito/:id",favoritoController.getFavorito)
+api.get("/favoritos",favoritoController.getFavoritos)
 api.post("/favorito",favoritoController.saveFavorito)
 api.put("/favorito",favoritoController.updateFavorito)
 api.delete("/favorito/:id",favoritoController.deleteFavorito)
